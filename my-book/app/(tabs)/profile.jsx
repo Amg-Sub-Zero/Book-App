0ec78fbec5aee9d7a5a62f2f5edb558468db8c0f
Amg-Sub-Zero <shamsudeenyakubu901@gmail.com>
2025-06-28 02:07:42 +0000

    my-book

--- my-book/app/(tabs)/profile.jsx
+++ my-book/app/(tabs)/profile.jsx
@@ -1,4 +1,11 @@
-import { View, Text, SafeAreaView, Image } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
 import React, { useCallback } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import CircularBackButton from "../components/CircularBackButton";
@@ -32,31 +39,196 @@ const ProfileHeader = () => (
   </View>
 );
 
+const EditProfile = () => (
+  <View>
+    <TouchableOpacity
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        marginHorizontal: 16,
+        marginTop: 20,
+      }}
+    >
+      <Ionicons name="person-outline" size={20} color="#333" />
+      <Text style={{ marginLeft: 12, fontSize: 16, fontWeight: "500" }}>
+        Edit Profile
+      </Text>
+      <Ionicons
+        name="chevron-forward"
+        size={20}
+        color="#333"
+        style={{ marginLeft: 220 }}
+      />
+    </TouchableOpacity>
+
+    <View
+      style={{
+        height: 1,
+        backgroundColor: "#e0e0e0",
+        marginHorizontal: 16,
+        marginTop: 20,
+      }}
+    />
+
+    <TouchableOpacity
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        marginHorizontal: 16,
+        marginTop: 20,
+      }}
+    >
+      <Ionicons name="radio-outline" size={20} color="#333" />
+      <Text style={{ marginLeft: 12, fontSize: 16, fontWeight: "500" }}>
+        My Adds
+      </Text>
+      <Ionicons
+        name="chevron-forward"
+        size={20}
+        color="#333"
+        style={{ marginLeft: 235 }}
+      />
+    </TouchableOpacity>
+
+    <View
+      style={{
+        height: 1,
+        backgroundColor: "#e0e0e0",
+        marginHorizontal: 16,
+        marginTop: 20,
+      }}
+    />
+
+    <TouchableOpacity
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        marginHorizontal: 16,
+        marginTop: 20,
+      }}
+    >
+      <Ionicons name="settings-outline" size={20} color="#333" />
+      <Text style={{ marginLeft: 12, fontSize: 16, fontWeight: "500" }}>
+        Settings
+      </Text>
+      <Ionicons
+        name="chevron-forward"
+        size={20}
+        color="#333"
+        style={{ marginLeft: 235 }}
+      />
+    </TouchableOpacity>
+
+    <View
+      style={{
+        height: 1,
+        backgroundColor: "#e0e0e0",
+        marginHorizontal: 16,
+        marginTop: 20,
+      }}
+    />
+
+    <TouchableOpacity
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        marginHorizontal: 16,
+        marginTop: 20,
+      }}
+    >
+      <Ionicons name="diamond-outline" size={20} color="#333" />
+      <Text style={{ marginLeft: 12, fontSize: 16, fontWeight: "500" }}>
+        Go Premium
+      </Text>
+      <Ionicons
+        name="chevron-forward"
+        size={20}
+        color="#333"
+        style={{ marginLeft: 210 }}
+      />
+    </TouchableOpacity>
+
+    <View
+      style={{
+        height: 1,
+        backgroundColor: "#e0e0e0",
+        marginHorizontal: 16,
+        marginTop: 20,
+      }}
+    />
+
+    <TouchableOpacity
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        marginHorizontal: 16,
+        marginTop: 20,
+      }}
+    >
+      <Ionicons name="log-out-outline" size={20} color="#ff4444" />
+      <Text
+        style={{
+          marginLeft: 12,
+          fontSize: 16,
+          fontWeight: "500",
+          color: "#ff4444",
+        }}
+      >
+        Log Out
+      </Text>
+      <Ionicons
+        name="chevron-forward"
+        size={20}
+        color="#ff4444"
+        style={{ marginLeft: 235 }}
+      />
+    </TouchableOpacity>
+
+    <View
+      style={{
+        height: 1,
+        backgroundColor: "#e0e0e0",
+        marginHorizontal: 16,
+        marginTop: 20,
+      }}
+    />
+  </View>
+);
+
 export default function Profile() {
   const router = useRouter();
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
-      <View
-        style={{
-          alignItems: "center",
-          padding: 16,
-          justifyContent: "space-between",
-        }}
-      >
-        <View>
-          <Text style={{ fontSize: 19, fontWeight: 10 }}>Profile</Text>
-        </View>
-        <View style={{ position: "absolute", left: 10, top: 10 }}>
-          <Text>
-            <CircularBackButton
-              onPress={() => router.back()}
-              style={{ position: "absolute", top: 40, left: 20 }}
-            />
-          </Text>
-        </View>
-        <ProfileHeader />
-      </View>
+      <FlatList
+        contentContainerStyle={{ paddingBottom: 24 }}
+        ListHeaderComponent={
+          <>
+            <View
+              style={{
+                alignItems: "center",
+                padding: 16,
+                justifyContent: "space-between",
+              }}
+            >
+              <View>
+                <Text style={{ fontSize: 19, fontWeight: 10 }}>Profile</Text>
+              </View>
+              <View style={{ position: "absolute", left: 10, top: 10 }}>
+                <Text>
+                  <CircularBackButton
+                    onPress={() => router.back()}
+                    style={{ position: "absolute", top: 40, left: 20 }}
+                  />
+                </Text>
+              </View>
+              <ProfileHeader />
+              <EditProfile />
+            </View>
+            ;
+          </>
+        }
+      />
     </SafeAreaView>
   );
 }
